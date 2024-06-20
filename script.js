879cbca373a9683413f4647a952d498c97a7420a
Shalakushka <56279749+Shalakushka@users.noreply.github.com>
2024-06-21 01:27:53 +0200

    Made the user interface to play the game
    - Reworked the algorithm to function with a  ui.
    - Integrated a display showing the game status,
    player score, computer score, and a round counter.

--- script.js
+++ script.js
@@ -1,58 +1,94 @@
+const humanScoreDiv = document.querySelector(".human-score");
+const computerScoreDiv = document.querySelector(".computer-score");
+const roundDiv = document.querySelector(".round");
+const statusDiv = document.querySelector(".status");
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorsButton = document.querySelector(".scissors")
+const resetButton = document.querySelector(".reset");
+let score = 0;
+let round = 1;
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock, Paper or Scissors ?");
-    return choice.toLowerCase();
-}
-
-function playRound(humanChoice, computerChoice) {
-    console.log(`You picked ${humanChoice}!\nYour opponent picked ${computerChoice}!`);
+function getResult(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
-        console.log("That's a tie!");
-        return;
+        return 0;
     }
     if (humanChoice == "rock") {
         if (computerChoice == "paper") {
-            console.log("You lose !");
             computerScore += 1;
         } else {
-            console.log("You win !");
             humanScore += 1;
         }
     } else if (humanChoice == "paper") {
         if (computerChoice == "scissors") {
-            console.log("You lose !");
             computerScore += 1;
         } else {
-            console.log("You win !");
             humanScore += 1;
         }
     } else if (humanChoice == "scissors") {
         if (computerChoice == "rock") {
-            console.log("You lose !");
             computerScore += 1;
         } else {
-            console.log("You win !");
             humanScore += 1;
         }
     }
 }
 
-function playGame() {
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    if (humanScore > computerScore) {
-        console.log("You won the game! Congratulations!");
-    } else {
-        console.log("The computer won... Maybe next time!");
-    }
+function resetGame() {
+    roundDiv.textContent = "1/5";
+    humanScoreDiv.textContent = "Your score : 0";
+    computerScoreDiv.textContent = "Opponent score : 0";
+    statusDiv.textContent = "Can you beat the computer ?";
+    humanScore = 0;
+    computerScore = 0;
+    round = 1;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    let result = getResult(humanChoice, computerChoice);
+
+    if (round == 6)
+        return;
+    if (result == 1)
+        statusDiv.textContent = `Your opponent picked ${computerChoice}! You won this round!`;
+    else if (result == -1)
+        statusDiv.textContent = `Your opponent picked ${computerChoice}! You lost this round!`;
+    else
+        statusDiv.textContent = `Your opponent picked ${computerChoice}! That's a tie...`;
+    round += 1;
+    if (round >= 6) {
+        if (humanScore > computerScore)
+            statusDiv.textContent = "You WON!";
+        else if (humanScore == computerScore)
+            statusDiv.textContent = "None of you won...";
+        else
+            statusDiv.textContent = "You lost to your opponent...";
+    } else {
+        score += result;
+        updateRound();
+    }
+    updateScore();
+}
+
+function updateRound() {
+    roundDiv.textContent = `${round}/5`;
+}
+
+function updateScore() {
+    humanScoreDiv.textContent = `Your score : ${humanScore}`;
+    computerScoreDiv.textContent = `Opponent score : ${computerScore}`;
+}
+
+rockButton.addEventListener("click", () => playRound("rock"));
+paperButton.addEventListener("click", () => playRound("paper"));
+scissorsButton.addEventListener("click", () => playRound("scissors"));
+resetButton.addEventListener("click", () => resetGame());
